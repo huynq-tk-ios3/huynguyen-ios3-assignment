1284e9ad32caa8eb53ee5f027c86adc58f7b3caf
huynq-tk-ios3 <huynq.work@gmail.com>
2016-05-08 16:05:41 +0700

    Fixed searching not show all words containing search phrase

--- Hakathon1/Tripsmore/DatabaseService.m
+++ Hakathon1/Tripsmore/DatabaseService.m
@@ -129,7 +129,7 @@ static id _instance = nil;
     if (!isEng2Pa) {
         strDB = @"table_pa_eng";
     }
-    NSString *strQuery = [NSString stringWithFormat:@"SELECT _id, word, result, description, favorites, edited FROM %@ WHERE word LIKE '%@%%' LIMIT 100", strDB, word];
+    NSString *strQuery = [NSString stringWithFormat:@"SELECT _id, word, result, description, favorites, edited FROM %@ WHERE word LIKE '%%%@%%' LIMIT 100", strDB, word];
     FMResultSet *results = [self.database executeQuery:strQuery];
     NSLog(@"query: %@", results.query);
     if (!results || !results.query) {
